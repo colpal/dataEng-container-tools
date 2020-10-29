20910c6f577b1579e2e093aa0151d616de34a8db
Alexander Saff <alexrsaff@gmail.com>
2020-10-29 16:47:56 +0000

    Revamp handling of secrets. By default now scans '/vault/secrets/' for any secret files and adds all values from there. Now allows multiple sercrets as inputs.

--- build/lib/dataEng_container_tools/__init__.py
+++ build/lib/dataEng_container_tools/__init__.py
@@ -1,4 +1,4 @@
-from .safe_stdout import setup_stdout
-from .cla import default_gcs_secret_location
+#python3 setup.py sdist bdist_wheel
+from .safe_stdout import setup_default_stdout
 
-setup_stdout(default_gcs_secret_location)
+setup_default_stdout()
