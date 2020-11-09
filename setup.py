d0413dd407f38403781b3a99899d4090980253a9
Alexander Saff <alexrsaff@gmail.com>
2020-11-09 16:56:45 +0000

    Build latest changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dataEng-container-tools",
-    version="0.0.10",
+    version="0.0.11",
     author="Alexander Saff",
     author_email="alexander_saff@colpal.com",
     description="A package containing tools for data engineering containers.",
