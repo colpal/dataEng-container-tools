3017637fbdfc7ce972f8551bdf8d76d086dccd09
Alexander Saff <alexrsaff@gmail.com>
2020-12-28 19:14:02 +0000

    Add preliminary Google style docstrings.

--- dataEng_container_tools/cla.py
+++ dataEng_container_tools/cla.py
@@ -1,3 +1,23 @@
+"""Tools for retrieving command line inputs.
+
+Deals with receiving input from the command line. Has three
+classes: `custom_command_line_argument`, `command_line_argument_type`,
+and `command_line_arguments`. `commnad_line_arguments` contains most
+of the functinality. `command_line_argument_type` is an enumeration.
+`custom_command_line_argument` is a wrapper for `parser.add_argument()`.
+
+  Typical usage example:
+
+    my_inputs = command_line_arguments(secret_locations=command_line_argument_type.OPTIONAL,
+                                    input_files=command_line_argument_type.REQUIRED,
+                                    output_files=command_line_argument_type.REQUIRED)
+
+    input_uris = my_inputs.get_input_uris()
+    output_uris = my_inputs.get_output_uris()
+    secret_locations = my_inputs.get_secret_locations()                              
+    file_io = gcs_file_io(gcs_secret_location = secret_locations[0])
+"""
+
 import argparse
 import json
 import sys
@@ -6,11 +26,45 @@ from .safe_stdout import setup_stdout, default_gcs_secret_locations, secrets_fil
 import os
 
 class custom_command_line_argument:
-    """ """
+    """Class for creating custom command line arguments.
+
+    This class is used for creating custom command line arguments. A
+    list of these objects can be passed into `command_line_arguments` which
+    will add them as command line arguments, parse the inputs, and return the results.
+    Objects of this class have all the same attributes available to `parser.add_argument()`.
+
+    Attributes:
+        name: A string to Argument name.
+        action: A string indicating the basic type of action to be taken when this argument is encountered at the command line.
+        nargs: An integer indicating the number of command-line arguments that should be consumed.
+        const: A constant value required by some action and nargs selections.
+        default: The value produced if the argument is absent from the command line and if it is absent from the namespace object.
+        data_type: The type to which the command-line argument should be converted.
+        choices: A container of the allowable values for the argument.
+        required: A boolean indicating whether or not the command-line option may be omitted (optionals only).
+        help_message: A string providing a brief description of what the argument does.
+        metavar: A string indicating the name for the argument in usage messages.
+        dest: A string indicating the name of the attribute to be added to the object returned by parse_args().
+    """
 
     def __init__(self, name, action = None, nargs = None, const = None, default = None,
                 data_type = None, choices = None, required = None, help_message = None,
                 metavar = None, dest = None):
+        """Initializes custom_command_line_arguments with desired configuration.
+
+        Args:
+            name: A string to Argument name.
+            action: A string indicating the basic type of action to be taken when this argument is encountered at the command line.
+            nargs: An integer indicating the number of command-line arguments that should be consumed.
+            const: A constant value required by some action and nargs selections.
+            default: The value produced if the argument is absent from the command line and if it is absent from the namespace object.
+            data_type: The type to which the command-line argument should be converted.
+            choices: A container of the allowable values for the argument.
+            required: A boolean indicating whether or not the command-line option may be omitted (optionals only).
+            help_message: A string providing a brief description of what the argument does.
+            metavar: A string indicating the name for the argument in usage messages.
+            dest: A string indicating the name of the attribute to be added to the object returned by parse_args().
+        """
         self.name = name
         self.action = action
         self.nargs = nargs
@@ -37,17 +91,63 @@ class custom_command_line_argument:
         "dest: " + self.dest)
 
 class command_line_argument_type(Enum):
-    """ """
+    """Enumeration class for use with command_line_arguments.
+    
+    Attributes:
+        OPTIONAL: For when a command line argument should be optional.
+        REQUIRED: For when a command line argument should be required.
+    """
     OPTIONAL = False
     REQUIRED = True
 
 class command_line_arguments:
-    """ """
+    """Creates, parses, and retrieves command line inputs.
+
+    This class creates command line arguments that are typically
+    used in Airflow containers. It will handle much of the backend
+    boilerplate code involved with creating, parsing, and storing
+    the relevant command line arguments using Python's argparse.
+    Includes helper functions for using the command line inputs.
+    """
+
     __default_secret_locations = default_gcs_secret_locations
+
     def __init__(self, input_files = None, output_files = None, secret_locations = None,
                 default_file_type = None, custom_inputs = None, description = None,
                 input_dtypes = None, running_local = None, identifying_tags = None,
                 parser = None):
+        """Initializes command_line_arguments with desired configuration.
+
+        Args:
+            input_files: Optional command_line_argument_type. Defaults to None. If REQUIRED,
+                will add --input_bucket_names, --input_paths, and --input_filenames as required
+                command line inputs. If OPTIONAL, will add them as optional. If None they will
+                not be added.
+            output_files: Optional command_line_argument_type. Defaults to None. If REQUIRED,
+                will add --output_bucket_names, --output_paths, and --output_filenames as required
+                command line inputs. If command_line_argument_type.OPTIONAL, will add them as
+                optional. If None they will not be added.
+            secret_locations: Optional command_line_argument_type. Defaults to None. If REQUIRED,
+                will add --gcs_secret_locations as required command line input. If OPTIONAL,
+                will add it as optional. If None, it will not be added.
+            default_file_type: Optional command_line_argument_type. Defaults to None. If REQUIRED,
+                will add --default_file_type as required command line argument. If OPTIONAL,
+                will add it as optional. Input can be one of parquet, csv, pkl, or json, with
+                the default being parquet. If None, the command line argument will not be added.
+            custom_inputs: Optional list of custom_command_line_arguments. Defaults to None.
+                All items in list will be added to the command line arguments.
+            description : Optional string. Defaults to None. A description to be printed when the
+                command line argument --help is used.
+            input_dtypes: Optional command_line_argument_type. Defaults to None. If input_files
+                is None, then this does nothing. If input_files is not None and input_dtypes
+                is REQUIRED, will add --input_dtypesas a required command line input. If OPTIONAL,
+                will add it as optional. Input is a JSON dictionary of (column: type) pairs.
+            parser: Optional argparse.ArgumentParser. Defaults to None. A parser on which to
+                add the command line arguments and parse. If None one will be created.
+            running_local: Optional boolean. Defaults to False. A flag for determining whether
+                or not the script is running locally and so should not attempt to contact GCP.
+                Defaults to False.
+        """
         self.__input_files = input_files
         self.__output_files = output_files
         self.__secret_locations = secret_locations
@@ -120,17 +220,31 @@ class command_line_arguments:
         return self.__args.__str__()
 
     def get_arguments(self):
-        """ """
+        """Retrieves the arguments passed in through the command line.
+        
+        Returns:
+            A Namespace object with all of the command line arguments.
+        """
         return self.__args
 
     def get_input_dtypes(self):
-        """ """
+        """Retrieves the input dtypes passed in through the command line.
+        
+        Returns:
+            None if dtypes were not asked for in intialization or the loaded JSON
+            object passed to input_dtypes through the command line otherwise.
+        """
         if not self.__input_dtypes:
             return None
         return self.__args.input_dtypes
 
     def get_input_uris(self):
-        """ """
+        """Retrieves the input URIs passed in through the command line.
+        
+        Returns:
+            A list of all input URIs passed in through the command line. URIs
+            are of the format 'gs://bucket_name/input_path/filename'.
+        """
         if not self.__input_files:
             return []
         constant_bucket = False
@@ -146,7 +260,12 @@ class command_line_arguments:
         return output
 
     def get_output_uris(self):
-        """ """
+        """Retrieves the output URIs passed in through the command line.
+        
+        Returns:
+            A list of all output URIs passed in through the command line. URIs
+            are of the format 'gs://bucket_name/input_path/filename'.
+        """
         if not self.__output_files:
             return []
         constant_bucket = False
@@ -162,7 +281,13 @@ class command_line_arguments:
         return output
 
     def get_secret_locations(self):
-        """ """
+        """Retrieves the secret file locations passed in through the command line.
+
+        Returns:
+            A list of all the secret file locations passed in through the command line. If
+            secret_locations was not asked for during initialization, returns a list of secrets
+            found automatically.
+        """
         if self.__secret_locations:
             return self.__args.secret_locations
         if len(secrets_files) > 0:
@@ -170,7 +295,14 @@ class command_line_arguments:
         return None
 
     def get_secrets(self):
-        """ """
+        """Retrieves the secrets loaded from files specified through the command line.
+
+        Returns:
+            A dictionary of all the secrets specified through the command line. If
+            secret_locations was not asked for during initialization, returns a dictionary
+            of secrets found automatically. The key is the name of the file with '.json' removed
+            and the value is the loaded secret file.
+        """
         return_list = {}
         secret_list = None
         if self.__secret_locations:
@@ -187,6 +319,7 @@ class command_line_arguments:
         return return_list
 
     def check_args(self):
-        """ """
+        """Ensures arguments are present and valid.
+        """
         #TODO: Implement this
         return
