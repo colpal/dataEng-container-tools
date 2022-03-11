7656f99d8652a9abe27ab283669d4bdd4e48a22a
Mourtallah Faye <serigne.faye@icloud.com>
2022-03-11 11:45:44 -0500

    update safe stout def with str casting

--- build/lib/dataEng_container_tools/safe_stdout.py
+++ build/lib/dataEng_container_tools/safe_stdout.py
@@ -100,8 +100,8 @@ def setup_stdout(secret_locations):
         these_bad_words = set(secret.values())
         bad_words.update(these_bad_words)
         for word in these_bad_words:
-            bad_words.add(json.dumps(word))
-            bad_words.add(json.dumps(word).encode('unicode-escape').decode())
+            bad_words.add(str(json.dumps(word)))
+            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
             bad_words.add(word.encode('unicode-escape').decode())
     sys.stdout.add_words(bad_words)
 
