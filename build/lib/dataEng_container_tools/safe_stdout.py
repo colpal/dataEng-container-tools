d6192a18bfd995a1a017dfe9c81b65fde5fa6ad1
Mourtallah Faye <serigne.faye@icloud.com>
2022-03-11 14:28:08 -0500

    cast secret to string

--- build/lib/dataEng_container_tools/safe_stdout.py
+++ build/lib/dataEng_container_tools/safe_stdout.py
@@ -27,7 +27,6 @@ secrets_files = []
 
 class safe_stdout:
     """Prints output with secrets removed.
-
     This class replaces sys.stdout so that when print or display
     calls are made this class is the one that processes them. The
     class maintains a list of 'bad_words' which are replaced with 
@@ -38,42 +37,39 @@ class safe_stdout:
 
     def __init__(self, bad_words):
         """Initializes safe_stdout with desired configuration.
-
         Args:
             bad_words: An iterable of words to censor from output.
         """
         self.__bad_words = {}
         for item in bad_words:
-            self.__bad_words[item] = len(item)
+            self.__bad_words[item] = len(str(item))
         self.__old_stdout = sys.stdout
 
     def write(self, message):
         """Outputs the desired message with secrets removed.
-
         Args:
           message: The message to output. Must either be a string or
             have a working __str__ method associated with it.
         """
         message = str(message)
         for bad_word in self.__bad_words:
-            bad_word_location = message.find(bad_word)
-            bad_word_length = self.__bad_words[bad_word]
+            bad_word_location = message.find(str(bad_word))
+            bad_word_length = self.__bad_words[str(bad_word)]
             while (bad_word_location != -1):
                 message = (message[0:bad_word_location] +
                            '*' * bad_word_length +
                            message[bad_word_location + bad_word_length:])
-                bad_word_location = message.find(bad_word)
+                bad_word_location = message.find(str(bad_word))
         self.__old_stdout.write(message)
 
     def add_words(self, bad_words):
         """Adds words to the list of words to censor from output.
-
         Args:
           bad_words: An iterable containing the words to
             add to the list of words to censor in output.
         """
         for item in bad_words:
-            self.__bad_words[item] = len(item)
+            self.__bad_words[item] = len(str(item))
 
     def flush(self):
         pass
@@ -81,12 +77,10 @@ class safe_stdout:
 
 def setup_stdout(secret_locations):
     """Adds the contents of a list of files to the words to censor from output.
-
     For each JSON location specified in the input, this method will open the JSON,
     parse its contents, and add the contents to the list of of words to censor from
     output. It adds the plaintext, the JSON loaded value, and the JSON dumped version of
     the value.
-
     Args:
       secret_locations: An iterable of strings representing the locations of JSON files
         whose contents should be censored from output.
@@ -100,15 +94,14 @@ def setup_stdout(secret_locations):
         these_bad_words = set(secret.values())
         bad_words.update(these_bad_words)
         for word in these_bad_words:
-            bad_words.add(json.dumps(word))
-            bad_words.add(json.dumps(word).encode('unicode-escape').decode())
-            bad_words.add(word.encode('unicode-escape').decode())
+            bad_words.add(str(json.dumps(word)))
+            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
+            bad_words.add(str(word).encode('unicode-escape').decode())
     sys.stdout.add_words(bad_words)
 
 
 def setup_default_stdout(folder=default_secret_folder):
     """Censors the contents of JSONs found in the secrets folder from output.
-
     Takes an argument that is the string representation of a folder present in
     the container. For each JSON in that folder, this method adds the values present
     to the list of words to censor from output. This method should only be called once,
@@ -116,7 +109,6 @@ def setup_default_stdout(folder=default_secret_folder):
     after importation will not cause errors, but will lead to unnecessary delay when attempting
     to output information. To add values to the list of words to censor from output, use either
     setup_stdout() or sys.stdout.add_words().
-
     Args:
       folder: A string representing the folder in which to look for secret JSONs. By default 
         the value is the  default_secret_folder global variable declared in this file.
@@ -137,7 +129,6 @@ def setup_default_stdout(folder=default_secret_folder):
         these_bad_words = set(secret.values())
         bad_words.update(these_bad_words)
         for word in these_bad_words:
-            bad_words.add(json.dumps(word))
-            bad_words.add(json.dumps(word).encode('unicode-escape').decode())
-            bad_words.add(word.encode('unicode-escape').decode())
-    sys.stdout = safe_stdout(bad_words)
+            bad_words.add(str(json.dumps(word)))
+            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
+            bad_words.add(str(word).encode(
