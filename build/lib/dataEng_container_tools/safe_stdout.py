0f163ad50b5c5edb67f6882867c94c1e9f9a366a
haitiankg92 <haitiankg92@gmail.com>
2022-05-10 11:07:41 -0400

    resolving conflicts

--- build/lib/dataEng_container_tools/safe_stdout.py
+++ build/lib/dataEng_container_tools/safe_stdout.py
@@ -138,7 +138,6 @@ def setup_default_stdout(folder=default_secret_folder):
         bad_words.update(these_bad_words)
         for word in these_bad_words:
             bad_words.add(str(json.dumps(word)))
-            bad_words.add(str(json.dumps(word)).encode(
-                'unicode-escape').decode())
+            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
             bad_words.add(str(word).encode('unicode-escape').decode())
     sys.stdout = safe_stdout(bad_words)
