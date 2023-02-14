32bebec46f12b0710d6d7589902d13a810cff708
Mourtallah Faye <serigne.faye@icloud.com>
2023-02-14 11:44:27 -0500

    update secret word extraction control flow

--- build/lib/dataEng_container_tools/safe_stdout.py
+++ build/lib/dataEng_container_tools/safe_stdout.py
@@ -21,7 +21,11 @@ import json
 import os
 
 default_secret_folder = '/vault/secrets/'
-default_secret_locations = [default_secret_folder + 'gcp-sa-storage.json', default_secret_folder + 'gcp-sa-bq.json']
+default_secret_locations = {
+    "GCS": default_secret_folder + 'gcp-sa-storage.json',
+    "BQ": default_secret_folder + 'gcp-sa-bq.json',
+    # "SF" : default_secret_folder + 'sf_creds.json'
+}
 secrets_files = []
 
 
@@ -95,14 +99,14 @@ def setup_stdout(secret_locations):
     for file in secret_locations:
         try:
             secret = json.load(open(file, 'r'))
+            these_bad_words = set(secret.values())
+            bad_words.update(these_bad_words)
+            for word in these_bad_words:
+                bad_words.add(str(json.dumps(word)))
+                bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
+                bad_words.add(str(word).encode('unicode-escape').decode())
         except ValueError:
             print(file, "is not a properly formatted json file.")
-        these_bad_words = set(secret.values())
-        bad_words.update(these_bad_words)
-        for word in these_bad_words:
-            bad_words.add(str(json.dumps(word)))
-            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
-            bad_words.add(str(word).encode('unicode-escape').decode())
     sys.stdout.add_words(bad_words)
 
 
@@ -131,13 +135,13 @@ def setup_default_stdout(folder=default_secret_folder):
     for file in files:
         try:
             secret = json.load(open(file, 'r'))
+            secrets_files.append(file)
+            these_bad_words = set(secret.values())
+            bad_words.update(these_bad_words)
+            for word in these_bad_words:
+                bad_words.add(str(json.dumps(word)))
+                bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
+                bad_words.add(str(word).encode('unicode-escape').decode())
         except ValueError:
             print(file, "is not a properly formatted json file.")
-        secrets_files.append(file)
-        these_bad_words = set(secret.values())
-        bad_words.update(these_bad_words)
-        for word in these_bad_words:
-            bad_words.add(str(json.dumps(word)))
-            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
-            bad_words.add(str(word).encode('unicode-escape').decode())
     sys.stdout = safe_stdout(bad_words)
