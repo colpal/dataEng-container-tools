b8bba4367f8f8c4886dfef50fba45c0c3c208ec7
Mourtallah Faye <serigne.faye@icloud.com>
2023-02-14 09:59:08 -0500

    update secret word extraction control flow

--- build/lib/dataEng_container_tools/safe_stdout.py
+++ build/lib/dataEng_container_tools/safe_stdout.py
@@ -135,13 +135,13 @@ def setup_default_stdout(folder=default_secret_folder):
     for file in files:
         try:
             secret = json.load(open(file, 'r'))
+            secrets_files.append(file)
+            these_bad_words = set(secret.values())
+            bad_words.update(these_bad_words)
+            for word in these_bad_words:
+                bad_words.add(str(json.dumps(word)))
+                bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
+                bad_words.add(str(word).encode('unicode-escape').decode())
         except ValueError:
             print(file, "is not a properly formatted json file.")
-        secrets_files.append(file)
-        these_bad_words = set(secret.values())
-        bad_words.update(these_bad_words)
-        for word in these_bad_words:
-            bad_words.add(str(json.dumps(word)))
-            bad_words.add(str(json.dumps(word)).encode('unicode-escape').decode())
-            bad_words.add(str(word).encode('unicode-escape').decode())
     sys.stdout = safe_stdout(bad_words)
